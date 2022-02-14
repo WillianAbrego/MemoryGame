2c39f630f0c1f4041df4a649f1993ab26e99e5d1
WillianAbrego <willianantonioabrego@gmail.com>
2022-02-14 13:10:47 -0600

    agregado iconos a cartas

--- docs/js/javascript.js
+++ docs/js/javascript.js
@@ -11,6 +11,16 @@ let DiferentesDificultades = {
   medio: [6, 18, "pokemon"],
   dificil: [8, 32, "coches"],
 };
+let icons = {
+  1: "fas fa-robot",
+  2: "fas fa-gamepad",
+  3: "fa fa-pencil",
+  4: "fas fa-ice-cream",
+  5: "fas fa-anchor",
+  6: "fas fa-bell",
+  7: "fas fa-key",
+  8: "far fa-money-bill-alt",
+};
 function grid() {
   // cargarImagenes();
   // getMaxPuntos();
@@ -37,15 +47,19 @@ const arrayOpciones = (array) => {
 
 const crearCarta = (cantidad) => {
   let cartas = cantidad * cantidad;
-  for (let i = 1; i <= cartas; i++) {
-    //console.log(i);
+  for (let i = 0; i < cartas; i++) {
+    console.log(i, icons[arr[i]]);
+    //console.log(arr[i]);
+    // console.log(icons[i]);
     document.getElementById(
       "principal"
     ).innerHTML += `<div class="row text-center">
     <div class="d-flex align-content-end flex-wrap contenido contenidoo" id="div1">
-      <div class="d-flex align-content-end flex-wrap contenido">
+      <div class="d-flex align-content-end flex-wrap contenido ">
         <div class="bg-white rounded shadow-sm py-5 px-4 contcard">
-          <h5 class="mb-0" title="f"><i class="fa fa-pencil extraClass"></i></h5>
+          <h5 class="mb-0" title="f"><i class=" ${
+            icons[arr[i]]
+          } extraClass"></i></h5>
           </button>
         </div>
       </div>
@@ -54,29 +68,34 @@ const crearCarta = (cantidad) => {
   }
 };
 
-// const obtenerNumeroAleatorioEnRango = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// };
-
-// // Probar con 10 números aleatorios entre 1 y 100
-// for (let i = 0; i < 9; i++) {
-//   const numeroAleatorio = obtenerNumeroAleatorioEnRango(1, 10);
-//   console.log("Aleatorio: ", numeroAleatorio);
-// }
-//Define la cantidad de numeros aleatorios.
-var cantidadNumeros = 10;
-var myArray = [];
-while (myArray.length < cantidadNumeros) {
-  var numeroAleatorio = Math.ceil(Math.random() * cantidadNumeros);
-  var existe = false;
-  for (var i = 0; i < myArray.length; i++) {
-    if (myArray[i] == numeroAleatorio) {
-      existe = true;
-      break;
+const ArrayNumAleatorio = (numeros) => {
+  let ArregloAleatorio = [];
+  while (ArregloAleatorio.length < numeros) {
+    let numeroAleatorio = Math.ceil(Math.random() * numeros);
+    let existe = false;
+    for (let i = 0; i < ArregloAleatorio.length; i++) {
+      if (ArregloAleatorio[i] == numeroAleatorio) {
+        existe = true;
+        break;
+      }
+    }
+    if (!existe) {
+      ArregloAleatorio[ArregloAleatorio.length] = numeroAleatorio;
     }
   }
-  if (!existe) {
-    myArray[myArray.length] = numeroAleatorio;
-  }
+  return ArregloAleatorio;
+};
+const PosicionCartas = () => {
+  let carta1 = ArrayNumAleatorio(8);
+  let carta2 = ArrayNumAleatorio(8);
+  carta1 = [...carta1, ...carta2];
+  return carta1;
+};
+
+function shuffleArray(inputArray) {
+  inputArray.sort(() => Math.random() - 0.5);
 }
-document.write("números aleatorios : " + myArray);
+let arr = PosicionCartas();
+shuffleArray(arr);
+console.log(arr);
+document.write(arr);
